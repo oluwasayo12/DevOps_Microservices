b81ff59e2c925872c80629f0ab02074e7e90d40c
EC2 Default User <ec2-user@ip-172-31-85-204.ec2.internal>
2022-07-29 16:57:02 +0000

    ready --one-last-step

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=phoenix5050/sklearn:v1.0
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag sklearn ${dockerpath}
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
